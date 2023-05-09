3b86374e5d8cc41fc190297aa97972f00dcb3042
Samuel Wölfl <samuelwoelfl@gmx.de>
2023-05-09 17:50:44 +0200

    updated for 6 channels

--- admin/admin-settings.php
+++ admin/admin-settings.php
@@ -21,6 +21,10 @@
             <select id="channel">
                 <option value="1">1</option>
                 <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
             </select>
         </div>
 
