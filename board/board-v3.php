df9a7890f426e50afb97ccde7e6c91e3aedbe5f1
Samuel Wölfl <samuelwoelfl@gmx.de>
2023-06-01 23:11:45 +0200

    optimized for smartphone

--- board/board-v3.php
+++ board/board-v3.php
@@ -11,9 +11,9 @@
 
         <div class="top">
             <div class="content">
-                <p class="text" id="Game">Game</p>
+                <p lang="de" class="text" id="Game">Game</p>
                 <span class="divider"></span>
-                <p class="text" id="Mode">Mode</p>
+                <p lang="de" class="text" id="Mode">Mode</p>
             </div>
         </div>
 
@@ -21,11 +21,11 @@
 
             <div class="team team_a">
                 <div class="name">
-                    <p class="teamname text" id="A_Teamname">A Teamname</p>
+                    <p lang="de" class="teamname text" id="A_Teamname">A Teamname</p>
                 </div>
                 <div class="score">
                     <div class="active_card">
-                      <p id="A_Score_Active">0</p>
+                      <p lang="de" id="A_Score_Active">0</p>
                     </div>
                     <div class="sets_card">
                       <span id="a_sets_won">0</span>
@@ -35,14 +35,14 @@
 
             <div class="team team_b">
                 <div class="name">
-                    <p class="teamname text" id="B_Teamname">B Teamname</p>
+                    <p lang="de" class="teamname text" id="B_Teamname">B Teamname</p>
                 </div>
                 <div class="score">
                   <div class="sets_card">
                     <span id="b_sets_won">0</span>
                   </div>
                   <div class="active_card">
-                    <p id="B_Score_Active">0</p>
+                    <p lang="de" id="B_Score_Active">0</p>
                   </div>
                 </div>
             </div>
@@ -58,58 +58,58 @@
         <div class="team_score">
             <div class="set active">
                 <div class="counter a_team set1">
-                    <p class="score" id="A_Score_1"></p>
+                    <p lang="de" class="score" id="A_Score_1"></p>
                 </div>
                 <div class="counter b_team set1">
-                    <p class="score" id="B_Score_1"></p>
+                    <p lang="de" class="score" id="B_Score_1"></p>
                 </div>
             </div>
             <div class="set">
                 <div class="counter a_team set2">
-                    <p class="score" id="A_Score_2"></p>
+                    <p lang="de" class="score" id="A_Score_2"></p>
                 </div>
                 <div class="counter b_team set2">
-                    <p class="score" id="B_Score_2"></p>
+                    <p lang="de" class="score" id="B_Score_2"></p>
                 </div>
             </div>
             <div class="set">
                 <div class="counter a_team set3">
-                    <p class="score" id="A_Score_3"></p>
+                    <p lang="de" class="score" id="A_Score_3"></p>
                 </div>
                 <div class="counter b_team set3">
-                    <p class="score" id="B_Score_3"></p>
+                    <p lang="de" class="score" id="B_Score_3"></p>
                 </div>
             </div>
             <div class="set">
                 <div class="counter a_team set4">
-                    <p class="score" id="A_Score_4"></p>
+                    <p lang="de" class="score" id="A_Score_4"></p>
                 </div>
                 <div class="counter b_team set4">
-                    <p class="score" id="B_Score_4"></p>
+                    <p lang="de" class="score" id="B_Score_4"></p>
                 </div>
             </div>
             <div class="set">
                 <div class="counter a_team set5">
-                    <p class="score" id="A_Score_5"></p>
+                    <p lang="de" class="score" id="A_Score_5"></p>
                 </div>
                 <div class="counter b_team set5">
-                    <p class="score" id="B_Score_5"></p>
+                    <p lang="de" class="score" id="B_Score_5"></p>
                 </div>
             </div>
             <div class="set">
                 <div class="counter a_team set6">
-                    <p class="score" id="A_Score_6"></p>
+                    <p lang="de" class="score" id="A_Score_6"></p>
                 </div>
                 <div class="counter b_team set6">
-                    <p class="score" id="B_Score_6"></p>
+                    <p lang="de" class="score" id="B_Score_6"></p>
                 </div>
             </div>
             <div class="set">
                 <div class="counter a_team set7">
-                    <p class="score" id="A_Score_7"></p>
+                    <p lang="de" class="score" id="A_Score_7"></p>
                 </div>
                 <div class="counter b_team set7">
-                    <p class="score" id="B_Score_7"></p>
+                    <p lang="de" class="score" id="B_Score_7"></p>
                 </div>
             </div>
         </div>
