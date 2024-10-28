84c00dc406ee5587ab8af8511f85c367270b45a2
Samuel Wölfl <68645378+samuelwoelfl@users.noreply.github.com>
2024-10-28 17:52:06 +0100

    removed comments

--- js/firebase_config.js
+++ js/firebase_config.js
@@ -6,8 +6,6 @@ import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/fir
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-var email = "test";
-var password = "test";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -25,22 +23,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// const auth = getAuth(app);
-// signInWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in 
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//   });
-
 export const db = getDatabase(app);
-// export const matchRef = ref(db, 'match');
-// export const teamsRef = ref(db, 'match/teams_info')
 
 
 
